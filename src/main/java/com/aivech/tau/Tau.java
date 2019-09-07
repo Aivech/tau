5a2a04af031e50959808c3562d8e79a4a546e93b
Aivech <16143392+Aivech@users.noreply.github.com>
2019-09-07 12:07:19 -0400

    Add creative tabs

--- src/main/java/com/aivech/tau/Tau.java
+++ src/main/java/com/aivech/tau/Tau.java
@@ -1,9 +1,15 @@
 package com.aivech.tau;
 
+import com.aivech.tau.block.BlockBase;
 import com.aivech.tau.block.TauBlocks;
+import com.aivech.tau.item.ItemBase;
 import com.aivech.tau.item.TauItems;
 import com.aivech.tau.tileentity.TauTileEntities;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +17,8 @@ public class Tau implements ModInitializer {
 
     public static Logger log;
     public static final String MODID = "tau";
+    public static ItemGroup blocks;
+    public static ItemGroup items;
 
     @Override
     public void onInitialize() {
@@ -19,5 +27,27 @@ public class Tau implements ModInitializer {
         TauBlocks.init();
         TauItems.init();
         TauTileEntities.init();
+
+        blocks = FabricItemGroupBuilder.create(
+                new Identifier(MODID,"blocks"))
+                .icon(()-> new ItemStack(TauBlocks.REGISTRY.get("testblock")))
+                .appendItems(stacks ->
+                {
+                    for (BlockBase b :TauBlocks.REGISTRY.values()) {
+                        stacks.add(new ItemStack(b));
+                    }
+                })
+                .build();
+
+        items = FabricItemGroupBuilder.create(
+                new Identifier(MODID,"items"))
+                .icon(()-> new ItemStack(TauItems.REGISTRY.get("testitem")))
+                .appendItems(stacks ->
+                {
+                    for (ItemBase i :TauItems.REGISTRY.values()) {
+                        stacks.add(new ItemStack(i));
+                    }
+                })
+                .build();
     }
 }
