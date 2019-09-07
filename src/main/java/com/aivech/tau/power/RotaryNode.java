8cc8f15b1089a1a461a4fa03364039049d20584e
Aivech <16143392+Aivech@users.noreply.github.com>
2019-09-07 19:54:52 -0400

    Grid node

--- src/main/java/com/aivech/tau/power/RotaryNode.java
+++ src/main/java/com/aivech/tau/power/RotaryNode.java
@@ -1,4 +1,16 @@
 package com.aivech.tau.power;
 
 public class RotaryNode {
+    public int speed;
+    public int torque;
+
+    public final NodeType type;
+
+    public RotaryNode(NodeType type) {
+        this.type = type;
+    }
+
+    public enum NodeType {
+        SOURCE, SINK, PATH
+    }
 }
