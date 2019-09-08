2c49512416dc3a3c4ea6e40aa102030b3914b0e4
Aivech <16143392+Aivech@users.noreply.github.com>
2019-09-08 18:11:43 -0400

    WorldUnloadEvent

--- src/main/java/com/aivech/tau/mixin/WorldUnloadEvent.java
+++ src/main/java/com/aivech/tau/mixin/WorldUnloadEvent.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(World.class)
 public class WorldUnloadEvent {
     @Inject(at = @At(value="INVOKE",
-            target = "Lnet/minecraft/world/chunk/ChunkManager;close()"),
+            target = "net/minecraft/world/chunk/ChunkManager.close()V"),
             method = "close"
     )
     private void tau_onWorldUnload(CallbackInfo ci){
